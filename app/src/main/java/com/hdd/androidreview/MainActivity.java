a40218e42586b7e471f07f0579bbd4548959646e
18931863853 <1073543127@qq.com>
2019-01-28 21:20:42 +0800

    android复习清单

---
+++ app/src/main/java/com/hdd/androidreview/MainActivity.java
@@ -0,0 +1,115 @@
+package com.hdd.androidreview;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private ListView mLV_Mian;
+    private List<String> itemList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "==onCreate()==");
+        mLV_Mian = findViewById(R.id.mLV_Mian);
+        itemList = new ArrayList<>();
+        itemList.add("生命周期");
+        itemList.add("启动模式");
+        MyListMain myListMain = new MyListMain(getApplicationContext(), itemList);
+        mLV_Mian.setAdapter(myListMain);
+    }
+
+
+    class MyListMain extends BaseAdapter {
+        private Context context;
+        private List<String> itemList;
+
+        public MyListMain(Context context, List<String> itemList) {
+            this.context = context;
+            this.itemList = itemList;
+        }
+
+        @Override
+        public int getCount() {
+            return itemList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            MyHolder myHolder = null;
+            //判断是否有缓存布局
+            if (convertView == null) {
+                convertView = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, null);
+                myHolder = new MyHolder(convertView);
+                convertView.setTag(position);
+            } else {
+                //得到缓存布局
+                myHolder = (MyHolder) convertView.getTag();
+            }
+            final String textContent = itemList.get(position);
+            myHolder.mTV_test1.setText(textContent);
+            myHolder.mTV_test1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent intent = new Intent();
+
+                    if ("生命周期".equals(textContent)) {
+                        intent.setClass(context, CycleActivity.class);
+                    } else if ("启动模式".equals(textContent)) {
+                        intent.setClass(context, PattermActivity.class);
+                    }
+                    ComponentName component = intent.getComponent();
+                    if (component == null) {
+                        Toast.makeText(context, "ComponentName为空", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    context.startActivity(intent);
+
+                }
+            });
+            return convertView;
+        }
+
+        class MyHolder {
+            TextView mTV_test1;
+
+            MyHolder(View view) {
+                mTV_test1 = view.findViewById(android.R.id.text1);
+            }
+
+
+        }
+    }
+
+}
---
+++ doc/android复习清单.md
@@ -0,0 +1,68 @@
+打算年假在家复习一下android的知识，就花了点时间列了一个复习的清单。清单中内个副标题上重要是自己复习的内容，下面并配有自己珍藏的需模块相关内容的连接。面对如今信息碎片化的信息时代，如果筛选和过滤听到的信息是一个困扰我们很久的问题。对于所以摆到到面前的资料不要马上去看，而是要看看他的评论，获赞等。(时间应该花在那些对自己有价值的东西上)
+
+#### **[掘金2018年度回顾](https://juejin.im/post/5c3ac44b51882553073fefb5#heading-33)**
+
+#### **[android知识库](https://github.com/francistao/LearningNotes)**
+
+#### 基础知识复习
+- [Activity生命周期相关的7个方法](https://www.cnblogs.com/joinrudy/p/3465442.html)
+1. 生命周期启动模式
+2. Fragment不同于Activity的生命周期
+3. 横竖屏及页面回收后恢复
+##### 自定义View
+
+- [自定义View系列](https://github.com/GcsSloop/AndroidNote/tree/master/CustomView)
+- [18年7月大神出版的《Android自定义控件入门与实战》](https://blog.csdn.net/harvic880925/article/details/81455073)
+1. 自定义view的坐标系
+2. view的绘制流程
+3. activity和window的关系
+
+##### 事件分发机制
+- [Android事件分发机制详解：史上最全面、最易懂](https://www.jianshu.com/p/38015afcdb58)
+- [图解 Android 事件分发机制](https://www.jianshu.com/p/e99b5e8bd67b)
+
+##### handler原理
+
+- [这是一份 全面、详细的Handler机制 学习攻略](https://www.jianshu.com/p/9fe944ee02f7)
+
+1. Handler发送消息有哪几种方式？
+2. Message、Handler、MessageQueue、Looper的之间的关系？
+3. 为什么在子线程中创建Handler会抛异常？
+
+##### 内存优化
+
+- [布局优化](https://www.jianshu.com/p/4e665e96b590)
+- [Android内存泄漏的八种可能](https://www.jianshu.com/p/ac00e370f83d)
+- [Android内存泄露 一篇文章就够了](https://www.jianshu.com/p/4ff69da351f6)
+
+#### android中的线程
+[从源码角度谈谈AsyncTask的使用及其原理](https://blog.csdn.net/u013637594/article/details/81698881)
+1. AsyncTask内部实现原理
+2. 多进程，如何开启多进程
+3. HandlerThread
+4. IntentService
+
+##### 动画
+- [Android 动画总结](https://www.jianshu.com/p/420629118c10)
+
+##### 适配
+
+- [Android屏幕适配姿势](https://www.jianshu.com/p/5d5494dd6dbe)
+- [Android 目前最稳定和高效的UI适配方案](https://juejin.im/post/5ae9cc3a5188253dc612842b)
+- [今日头条适配方案](https://juejin.im/post/5b7a29736fb9a019d53e7ee2)
+
+##### android 面试整理
+- [今年Android面试经历的一些总结--18年，有见了指导](https://www.jianshu.com/p/ffe7e0561c85)
+- [2018年Android面试题整理](https://juejin.im/post/5a82a07df265da4e7071c78f)
+
+- [我所经历的Android面试](https://juejin.im/post/5ab7a9cd6fb9a028c812d24b)
+- [Android面试题整理](https://www.jianshu.com/p/a22450882af2)
+
+##### 其他
+- [Android源码设计模式分析项目](https://github.com/simple-android-framework-exchange/android_design_patterns_analysis#schedule)
+- [Java数据结构-数组](https://juejin.im/post/5a65ffaff265da3e330475ad)
+- [数据结构中关于树的一切（java版）](https://juejin.im/post/5ad56de7f265da2391489be3)
+- [看得见的数据结构Android版之单链表篇](https://juejin.im/post/5bf8be9251882516be2ed1ad)
+1. 常见的设计模式
+2. 数组
+3. 链表
